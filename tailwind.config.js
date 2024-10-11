51ccfdccd7e06ef582fdd0797d717e805e94150e
Naeem Ashraf <123843315+NP5555@users.noreply.github.com>
2024-10-10 21:42:35 -0700

    "mde chngs

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all React component files
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        "fade-in-up": {
+          "0%": {
+            opacity: 0,
+            transform: "translate3d(0, 100%, 0)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate3d(0, 0, 0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-up": "fade-in-up 0.5s ease-out",
+      },
+    },
+  },
+  plugins: [],
+};
